3b3ad6be50ab89576073b8c818cd6c82539e5961
M.F.M <mfmfazrin1986@gmail.com>
2025-05-03 02:02:10 +0300

    Increase footer logo height from 10px to 12px

--- src/components/footer/FooterLogo.tsx
+++ src/components/footer/FooterLogo.tsx
@@ -13,7 +13,7 @@ export const FooterLogo: React.FC = () => {
         <img
           src="https://ik.imagekit.io/quadrate/QTS%20Logo%20Primary.png?updatedAt=1733854434969"
           alt="QTS Logo"
-          className="h-10 w-auto brightness-[0.7] contrast-[1.4] [filter:saturate(1.2)_hue-rotate(-10deg)] border-2 border-white rounded-md p-1 bg-white"
+          className="h-12 w-auto brightness-[0.7] contrast-[1.4] [filter:saturate(1.2)_hue-rotate(-10deg)] border-2 border-white rounded-md p-1 bg-white"
         />
         <span className="ml-2 text-xl font-bold text-[#FFFFFF]">QTS</span>
       </Link>
