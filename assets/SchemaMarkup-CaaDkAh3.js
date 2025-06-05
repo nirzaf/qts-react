d160609ad307f57cf30bfdaaee5bb21f70087ee7
nirzaf <mfmfazrin1986@gmail.com>
2025-06-05 10:05:38 +0000

    Deploying to gh-pages from @ nirzaf/qts-react@41bee1f2bbdd85506630a6d7d7a6cea9ff6f8a50 🚀

--- assets/SchemaMarkup-CaaDkAh3.js
+++
@@ -1,6 +0,0 @@
-import{j as r}from"./framer-motion-DXVt6-Fp.js";import{c as i,a as s}from"./index-v29Kktax.js";/**
- * @license lucide-react v0.503.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const n=[["path",{d:"M21 12a9 9 0 1 1-6.219-8.56",key:"13zald"}]],c=i("loader-circle",n),p=()=>r.jsx("div",{className:"flex min-h-[200px] w-full items-center justify-center",children:r.jsx(c,{className:"h-8 w-8 animate-spin text-primary"})}),d=({schema:t})=>{const e=()=>Array.isArray(t)?t.map(a=>r.jsx("script",{type:"application/ld+json",children:JSON.stringify(a)},`ld-${Math.random().toString(36).substr(2,9)}`)):r.jsx("script",{type:"application/ld+json",children:JSON.stringify(t)});return r.jsx(s,{children:e()})};export{p as L,d as S};
